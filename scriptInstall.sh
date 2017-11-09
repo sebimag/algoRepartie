814c219a572539364cb8757f3ed05fdc7105482d
Sebastien Anouilh <sebastien.anouilh@ensimag.grenoble-inp.fr>
2017-11-09 11:45:41 +0100

    correction

--- scriptInstall.sh
+++ scriptInstall.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 #GET ENV
-touch hostfile
+touch x10/hostfile.txt
 
 export JAVA_HOME='/usr/lib/jvm/java-7-openjdk-amd64/'
 
 
 nbNodes=$(uniq $OAR_NODEFILE | wc -l)
-uniq $OAR_NODEFILE > hostfile
+uniq $OAR_NODEFILE > x10/hostfile.txt
 
-export X10_HOSTFILE='hostfile'
+export X10_HOSTFILE='hostfile.txt'
 export X10_NPLACES=$nbNodes
 
 x10/bin/x10c hello.x10
