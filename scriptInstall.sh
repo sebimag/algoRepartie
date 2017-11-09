b809e0a494de9cc2c5af4ae1372776195212013a
Sebastien Anouilh <sebastien.anouilh@ensimag.grenoble-inp.fr>
2017-11-09 11:35:15 +0100

    correction

--- scriptInstall.sh
+++ scriptInstall.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
 #GET ENV
-wget https://sourceforge.net/projects/x10/files/x10/2.6.1/x10-2.6.1_linux_x86_64.tgz
+#wget https://sourceforge.net/projects/x10/files/x10/2.6.1/x10-2.6.1_linux_x86_64.tgz
 
-mkdir x10
-
-tar -xvf x10-2.6.1_linux_x86_64.tgz -C x10
+#mkdir x10
+touch hostfile
+#tar -xvf x10-2.6.1_linux_x86_64.tgz -C x10
 
 export JAVA_HOME='/usr/lib/jvm/java-7-openjdk-amd64/'
 
 
 nbNodes=$(uniq $OAR_NODEFILE | wc -l)
-nodes=$(uniq $OAR_NODEFILE)
+uniq $OAR_NODEFILE > hostfile
 
-export X10_HOSTFILE=$nodes
+export X10_HOSTFILE='hostfile'
 export X10_NPLACES=$nbNodes
-
-source /etc/profile
 
 x10/bin/x10c hello.x10
 
