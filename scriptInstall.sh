8e5ed6d00ae533ef69af04db510a5ab985f9410a
Sebastien Anouilh <sebastien.anouilh@ensimag.grenoble-inp.fr>
2017-11-09 10:54:57 +0100

    add

---
+++ scriptInstall.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#GET ENV
+wget https://sourceforge.net/projects/x10/files/x10/2.6.1/x10-2.6.1_linux_x86_64.tgz
+
+mkdir x10
+
+tar -xvf x10-2.6.1_linux_x86_64.tgz -C x10
+
+export JAVA_HOME='/usr/lib/jvm/java-7-openjdk-amd64/'
+
+
+nbNodes = uniq $OAR_NODEFILE | wc -l
+nodes = uniq $OAR_NODEFILE
+
+export X10_HOSTFILE=$nodes
+export X10_NPLACES=$nbNodes
+
+source .bashrc
+
+x10/bin/x10c Hello.x10
+
+x10/bin/x10 Hello
