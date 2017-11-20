690fec97c190e47ca3868fdf18b60fffe38835b1
Sebastien Anouilh <sebastien.anouilh@ensimag.grenoble-inp.fr>
2017-11-20 15:44:22 +0100

    jj

---
+++ scriptStartTest.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "1. Test premier"
+echo "2. other Test"
+echo "3. another Test"
+read -p "Select num test : " numTest
+
+case $numTest in
+        1)
+	gcc testMakefiles/premier/premier.c -lm -o premier
+	./scriptSend2Slave.sh testMakefiles/premier/premier
+	cp testMakefiles/premier/Makefile DistMake
+	cd  DistMake
+	~/x10/bin/x10 DistMake Makefile list.txt
+	;;
+        2) echo "Choix 2"
+	;;
+	3) echo "Choix 3"
+	;;
+        *)
+esac
+
+
+
+
