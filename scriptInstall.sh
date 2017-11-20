2513d7ee15b4c8354e8bd34ea15caad11c1803f1
Sebastien Anouilh <sebastien.anouilh@ensimag.grenoble-inp.fr>
2017-11-20 11:08:00 +0100

    gg

--- scriptInstall.sh
+++ scriptInstall.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
-
+#source ./scriptInstall.sh
 #GET ENV
+cd /tmp
 touch hostfile.txt
-
 export JAVA_HOME='/usr/lib/jvm/java-7-openjdk-amd64/'
 
 nbNodes=$(uniq $OAR_NODEFILE | wc -l)
-uniq $OAR_NODEFILE > /tmp/projet/hostfile.txt
+uniq $OAR_NODEFILE > hostfile.txt
 
-export X10_HOSTFILE='/tmp/projet/hostfile.txt'
+export X10_HOSTFILE='hostfile.txt'
 export X10_NPLACES=$nbNodes
-
-x10/bin/x10c DistMake/src/DistMake.x10
-
-filename="${file%.*}"
-x10/bin/x10 DistMake Makefile.txt fin
+export X10_ABSOLUTEPATH=$(pwd)/hostfile.txt
