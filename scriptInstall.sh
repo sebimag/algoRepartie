a1886591964ea91c3474620c6279f8178b38ce6b
Sebastien Anouilh <sebastien.anouilh@ensimag.grenoble-inp.fr>
2017-11-20 13:59:22 +0100

    man

--- scriptInstall.sh
+++ scriptInstall.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 #source ./scriptInstall.sh
 #GET ENV
-mkdir -p /tmp/projet/
-cd /tmp/projet/
-touch hostfile.txt
-export JAVA_HOME='/usr/lib/jvm/java-7-openjdk-amd64/'
+userHome="/home/ndekerleau"
+cd /tmp/
+mkdir -p "$userHome/DistMake"
+touch "$userHome/hostfile.txt"
+export JAVA_HOME="/usr/lib/jvm/java-7-openjdk-amd64/"
 
 nbNodes=$(uniq $OAR_NODEFILE | wc -l)
-uniq $OAR_NODEFILE > hostfile.txt
+uniq $OAR_NODEFILE > "$userHome/hostfile.txt"
 
-export X10_HOSTFILE='/tmp/projet/hostfile.txt'
+export X10_HOSTFILE="$userHome/hostfile.txt"
 export X10_NPLACES=$nbNodes
-export X10_ABSOLUTEPATH='/tmp/projet/hostfile.txt'
+export X10_ABSOLUTEPATH="$userHome/hostfile.txt"
 
-~/x10/bin/x10c DistMake/src/DistMake.x10
+file="$userHome/hostfile.txt"
+i=0
+while IFS= read -r line
+do
+	if [ $i -gt 0 ]
+	then
+		scp -r /tmp/DistMake/ $line:/tmp/
+	fi
+	((i++))
+done < "$file"
+
+
+~/x10/bin/x10c -d "$userHome"/DistMake DistMake/src/DistMake.x10
